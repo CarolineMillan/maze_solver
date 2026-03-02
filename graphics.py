aa61680912420e29925fc4491b57c79c5a3a14fe
carolinemillan <carolinemillan5678@gmail.com>
2026-03-02 17:28:58 +0000

    made fields and methods private

--- graphics.py
+++ graphics.py
@@ -2,28 +2,28 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.root_widget = Tk()
-        self.root_widget.title = "Maze Solver"
-        self.canvas_widget = Canvas(self.root_widget, bg="white", height=height, width=width)
-        self.canvas_widget.pack(fill=BOTH, expand=1)
-        self.running = False
+        self.__root_widget = Tk()
+        self.__root_widget.title = "Maze Solver"
+        self.__canvas_widget = Canvas(self.__root_widget, bg="white", height=height, width=width)
+        self.__canvas_widget.pack(fill=BOTH, expand=1)
+        self.__running = False
 
-        self.root_widget.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root_widget.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        self.root_widget.update_idletasks()
-        self.root_widget.update()
+        self.__root_widget.update_idletasks()
+        self.__root_widget.update()
 
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self.__running = True
+        while self.__running:
             self.redraw()
 
     def close(self):
-        self.running = False
+        self.__running = False
 
     def draw_line(self, line, fill_colour):
-        line.draw(self.canvas_widget, fill_colour)
+        line.draw(self.__canvas_widget, fill_colour)
 
 class Point:
     def __init__(self, x, y):
