441c09573c873942f22b8467b76b235cc0e1f118
carolinemillan <carolinemillan5678@gmail.com>
2026-03-05 17:58:24 +0000

    added break walls method, which turns the grid into a maze

--- maze.py
+++ maze.py
@@ -1,5 +1,6 @@
 from cell import Cell
 from time import sleep
+from random import seed, randrange
 
 class Maze:
     def __init__(
@@ -10,7 +11,8 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
-        win=None
+        win=None,
+        seed=None
     ):
         self.__x1 = x1
         self.__y1 = y1
@@ -19,9 +21,15 @@ class Maze:
         self.__cell_size_x = cell_size_x
         self.__cell_size_y = cell_size_y
         self.__win = win
+        if seed is None:
+            self.__seed = None
+        else:
+            self.__seed = seed(seed)
         self.__cells = []
         self.__create_cells()
         self.__break_entrance_and_exit()
+        self.__break_walls_r(0,0)
+        self.__reset_cells_visited()
 
     def __create_cells(self):
         # create a num_cols x num_rows sized 2D array of Cell objects
@@ -43,12 +51,66 @@ class Maze:
     def __animate(self):
         if self.__win != None:
             self.__win.redraw()
-            sleep(0.01)
+            sleep(0.001)
 
     def __break_entrance_and_exit(self):
         last_col = self.__num_cols - 1
         last_row = self.__num_rows - 1
+        if len(self.__cells) == 0:
+            return
         self.__cells[0][0].has_top_wall = False
         self.__draw_cell(0,0)
         self.__cells[last_col][last_row].has_bottom_wall = False
         self.__draw_cell(last_col, last_row)
+
+    def __break_walls_r(self, i, j):
+        
+        if len(self.__cells) == 0:
+            return
+        
+        self.__cells[i][j].visited = True
+
+        while True:
+            to_be_visited = []
+
+            # check adjacent cells, make sure they exist
+            if (i > 0) and (self.__cells[i-1][j].visited == False):
+                to_be_visited.append([i-1, j, "left"])
+            if (i < self.__num_cols - 1) and (self.__cells[i+1][j].visited == False):
+                to_be_visited.append([i+1, j, "right"])
+            if (j < self.__num_rows - 1) and (self.__cells[i][j+1].visited == False):
+                to_be_visited.append([i, j+1, "bottom"])
+            if (j > 0) and (self.__cells[i][j-1].visited == False):
+                to_be_visited.append([i, j-1, "top"])
+
+            # check to see if there's anywhere we can visit
+            if len(to_be_visited) == 0:
+                # draw the current cell
+                self.__draw_cell(i, j)
+                return
+            
+            next_cell = randrange(len(to_be_visited))
+
+            next_i, next_j, direction = to_be_visited[next_cell]
+            
+            # knock down the wall between the current cell and the chosen cell
+            match direction:
+                case "left":
+                    self.__cells[i][j].has_left_wall = False
+                    #self.__cells[next_i][next_j].has_right_wall = False
+                case "right":
+                    self.__cells[i][j].has_right_wall = False
+                case "bottom":
+                    self.__cells[i][j].has_bottom_wall = False
+                case  "top":
+                    self.__cells[i][j].has_top_wall = False
+                
+            self.__draw_cell(i, j)
+
+            # move to the chosen cell
+            self.__break_walls_r(next_i, next_j)
+
+    def __reset_cells_visited(self):
+        for column in self.__cells:
+            for cell in column:
+                cell.visited = False
--- tests.py
+++ tests.py
@@ -29,8 +29,8 @@ class Tests(unittest.TestCase):
         )
     
     def test_maze_create_cells_large(self):
-        num_cols = 200
-        num_rows = 300
+        num_cols = 20
+        num_rows = 30
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
             len(m1._Maze__cells),
@@ -54,6 +54,18 @@ class Tests(unittest.TestCase):
             m1._Maze__cells[11][15].has_bottom_wall,
             False,
         )
+    
+    def test_maze_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 16
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        m1._Maze__reset_cells_visited()
+        for col in m1._Maze__cells:
+            for cell in col:
+                self.assertEqual(
+                    m1._Maze__cells[0][0].visited,
+                    False,
+                )
 
 if __name__ == "__main__":
     unittest.main()
